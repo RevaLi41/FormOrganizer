98c065e1acb88976d153c95e776b097e7e5790d0
Reva Li <revacholianlibertine@gmail.com>
2025-07-13 19:38:17 -0400

    kdsjfhdajsk

---
+++ const.py
@@ -0,0 +1,8 @@
+
+
+class CONST:
+    def _init(self):
+        self.ID_FRONT = f'{None}_{None}_ID_Front.{None}'
+        self.ID_BACK = f'{None}_{None}_ID_Back.{None}'
+        self.PERSON_PLUS_ID = f'{None}_{None}__ID.{None}'
+        self.PERSON_PLUS_ID_PLUS_FORM = f'{None}_{None}__ID__{None}.{None}'
